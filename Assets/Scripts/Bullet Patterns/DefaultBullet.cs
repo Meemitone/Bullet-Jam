e3e3abd7dad4ea3d5916711ed6e721a803cd9980
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-01-30 13:49:51 +0000

    Groot Patterns done bar 1 minor error

--- Assets/Scripts/Bullet Patterns/DefaultBullet.cs
+++ Assets/Scripts/Bullet Patterns/DefaultBullet.cs
@@ -8,15 +8,22 @@ public class DefaultBullet : MonoBehaviour
     [SerializeField] public float Speed;
     [Tooltip("Direction of the projectile (calculated)")]
     [SerializeField] public Vector3 Direction;
+    public float MaxTravel = 100;
+    private Vector3 start;
     // Start is called before the first frame update
     void Start()
     {
+        start = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition += Direction * Time.deltaTime;
+        transform.localPosition += Direction * Time.deltaTime * Speed;
+        if(Vector3.Distance(transform.position, start) > MaxTravel)
+        {
+            Destroy(gameObject);
+        }
     }
 
     /*
