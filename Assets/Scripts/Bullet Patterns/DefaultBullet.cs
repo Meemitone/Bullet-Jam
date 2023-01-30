8f2cf709111b7ed2a999edd148b600ed8b0894d2
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-01-30 09:13:10 +0000

    Added Shotgun Bullet Spread Pattern

--- Assets/Scripts/Bullet Patterns/DefaultBullet.cs
+++ Assets/Scripts/Bullet Patterns/DefaultBullet.cs
@@ -5,35 +5,18 @@ using UnityEngine;
 public class DefaultBullet : MonoBehaviour
 {
     [Tooltip("Speed of the projectile in units/second")]
-    [SerializeField] private float Speed;
+    [SerializeField] public float Speed;
     [Tooltip("Direction of the projectile (calculated)")]
-    [SerializeField] private Vector3 Direction;
-    [Tooltip("Player GameObject (found by script)")]
-    [SerializeField] private GameObject player;
+    [SerializeField] public Vector3 Direction;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] playerGroup = FindObjectsOfType<GameObject>();
-        foreach(GameObject g in playerGroup)
-        {
-            if (g.CompareTag("Player"))
-                player = g;
-        }
-        if(player == null)
-        {
-            Debug.Log("Bullet can't find player");
-            Destroy(this.gameObject);
-        }
-        Direction = player.transform.position - transform.position;
-        Direction.y = 0;
-        Direction.Normalize();
-        Direction *= Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Direction * Time.deltaTime;
+        transform.localPosition += Direction * Time.deltaTime;
     }
 
     /*
--- Assets/Scripts/ShotgunBulleter.cs
+++ Assets/Scripts/ShotgunBulleter.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DefaultBulletPatternController : MonoBehaviour
+public class ShotgunBulleter : MonoBehaviour
 {
-    [Header("This Bullet Pattern Script Does Nothing")]
-    public int dummy;
-
+    [Header("This Bullet Pattern Script Fires projectiles in a fan arc, with the center aimed at the player")]
+    public float Spread;
     public float BulletSpeeds;
+    public int BulletCount;
+    public GameObject BulletType;
     [Header("This entity spawned these bullets (assigned by this script)")]
     public Gun_Controller Spawner;
     // Start is called before the first frame update
     void Start()
     {
+        if(transform.parent!=null)
         Spawner = transform.parent.parent.GetComponentInChildren<Gun_Controller>();
         GameObject[] Group = FindObjectsOfType<GameObject>();
-        GameObject player = null;
+        GameObject player=null;
         foreach (GameObject g in Group)
         {
             if (g.CompareTag("Player"))
@@ -30,19 +32,27 @@ public class DefaultBulletPatternController : MonoBehaviour
         Direction.y = 0;
         Direction.Normalize();
         transform.rotation = Quaternion.LookRotation(Direction, Vector3.up);
-        DefaultBullet[] childs = GetComponentsInChildren<DefaultBullet>();
-        foreach(DefaultBullet boolet in childs)
+        Spread *= Mathf.Deg2Rad; 
+        float cA = -Spread / 2; //currentAngle
+        for (int i = 0; i < BulletCount; i++)
         {
-            boolet.Speed = BulletSpeeds;
-            boolet.Direction = new Vector3(0,0,1);
+            GameObject newbullet = Instantiate(BulletType, transform.position, Quaternion.identity, transform);
+            Vector3 tempDir = new Vector3(Mathf.Sin(Mathf.Asin(0)+cA) ,0 , Mathf.Cos(Mathf.Asin(0) + cA));
+            newbullet.transform.rotation = Quaternion.LookRotation(tempDir, Vector3.up);
+            cA += Spread / (BulletCount-1);
+            newbullet.GetComponent<DefaultBullet>().Speed = BulletSpeeds;
+            newbullet.GetComponent<DefaultBullet>().Direction = tempDir;
         }
+
+
+        if (Spawner!=null)
         Spawner.CompleteFire();//this bullet pattern is so simple that it's finished firing immediately, there's no circular pattern spawned one at a time or something
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.childCount == 0)
+        if (transform.childCount == 0)
         {
             Destroy(gameObject);
         }
