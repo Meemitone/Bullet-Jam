bacc07dece9a243790dd43a34981cf2f4204ad3b
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-01-31 09:37:46 +0000

    Bullets Completed

--- Assets/Scripts/Bullet Patterns/ShotgunBulleter.cs
+++ Assets/Scripts/Bullet Patterns/ShotgunBulleter.cs
@@ -11,13 +11,14 @@ public class ShotgunBulleter : MonoBehaviour
     public GameObject BulletType;
     [Header("This entity spawned these bullets (assigned by this script)")]
     public Gun_Controller Spawner;
+    public bool overrider = false;
     // Start is called before the first frame update
     void Start()
     {
-        if(transform.parent!=null)
-        Spawner = transform.parent.parent.GetComponentInChildren<Gun_Controller>();
+        if (transform.parent != null)
+            Spawner = transform.parent.parent.GetComponentInChildren<Gun_Controller>();
         GameObject[] Group = FindObjectsOfType<GameObject>();
-        GameObject player=null;
+        GameObject player = null;
         foreach (GameObject g in Group)
         {
             if (g.CompareTag("Player"))
@@ -28,25 +29,32 @@ public class ShotgunBulleter : MonoBehaviour
             Debug.Log("Bullet can't find player");
             Destroy(this.gameObject);
         }
-        Vector3 Direction = player.transform.position - transform.position;
-        Direction.y = 0;
-        Direction.Normalize();
-        transform.rotation = Quaternion.LookRotation(Direction, Vector3.up);
-        Spread *= Mathf.Deg2Rad; 
+        if (!overrider)
+        {
+            Vector3 Direction = player.transform.position - transform.position;
+            Direction.y = 0;
+            Direction.Normalize();
+            transform.rotation = Quaternion.LookRotation(Direction, Vector3.up);
+        }
+        else
+        {
+
+        }
+        Spread *= Mathf.Deg2Rad;
         float cA = -Spread / 2; //currentAngle
         for (int i = 0; i < BulletCount; i++)
         {
             GameObject newbullet = Instantiate(BulletType, transform.position, Quaternion.identity, transform);
-            Vector3 tempDir = new Vector3(Mathf.Sin(Mathf.Asin(0)+cA) ,0 , Mathf.Cos(Mathf.Asin(0) + cA));
+            Vector3 tempDir = new Vector3(Mathf.Sin(Mathf.Asin(0) + cA), 0, Mathf.Cos(Mathf.Asin(0) + cA));
             newbullet.transform.rotation = Quaternion.LookRotation(tempDir, Vector3.up);
-            cA += Spread / (BulletCount-1);
+            cA += Spread / (BulletCount - 1);
             newbullet.GetComponent<DefaultBullet>().Speed = BulletSpeeds;
             newbullet.GetComponent<DefaultBullet>().Direction = tempDir;
         }
 
 
-        if (Spawner!=null)
-        Spawner.CompleteFire();//this bullet pattern is so simple that it's finished firing immediately, there's no circular pattern spawned one at a time or something
+        if (Spawner != null)
+            Spawner.CompleteFire();//this bullet pattern is so simple that it's finished firing immediately, there's no circular pattern spawned one at a time or something
     }
 
     // Update is called once per frame
