26edf75db5928ab535e44e7d1a0688a11d42689f
Grumtom <Tomgrumley2002@gmail.com>
2023-02-02 16:11:37 +0000

    Bullets and chainsaw

--- Assets/Scripts/Player/Gun Bullets/ChainsawControl.cs
+++ Assets/Scripts/Player/Gun Bullets/ChainsawControl.cs
@@ -7,17 +7,18 @@ public class ChainsawControl : MonoBehaviour
 
     public GameObject player;
     public BoxCollider chainsaw;
+    public ParticleSystem chainsawSparks;
 
     public void RevSaw()
     {
         chainsaw.enabled = true;
-
+        chainsawSparks.gameObject.SetActive(true);
 
     }
 
     public void StopSaw()
     {
-
+        chainsawSparks.gameObject.SetActive(false);
         chainsaw.enabled = false;
         player.GetComponent<PlayerGun>().chainsawing = false;
 
